295f288a19ebef82bdd04c1c644e18e485e6ed67
John Austeen <shajohn30@gmail.com>
2022-11-04 22:32:13 -0400

    Create lova_base.py
    class file for lova_base

---
+++ lova_base.py
@@ -0,0 +1,20 @@
+class lova_base:
+    def __init__(self, camerax,cameraf1,cameraf2,irs1, irs2, motorA, motorB, wheelsetA, wheelsetB, battery_unit, control_unit, wireless_module ):
+        self.camerax = camerax
+        self.cameraf1 = cameraf1
+        self.cameraf2 = cameraf2
+        self.irs1 = irs1
+        self.irs2 = irs2
+        self.motorA = motorA
+        self.motorB = motorB
+        self.wheelsetA = wheelsetA
+        self.wheelsetB = wheelsetB
+        self.battery_unit = battery_unit
+        self.control_unit = control_unit
+        self.wireless_module = wireless_module
+        
+    def control_1_operation(x):
+        print('The control_1 operation is started')
+
+    def control_2_operation(x):
+        print('The control_2 operation is started')
